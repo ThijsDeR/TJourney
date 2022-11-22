057d6e1680455889eaf51d9d78a57db61855dc8a
Mitchel0058 <goed0058@hz.nl>
2022-11-22 17:08:15 +0100

    post working

--- src/pages/api/testApi.js
+++ src/pages/api/testApi.js
@@ -6,26 +6,49 @@ function Api() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [swapiItems, setSwapiItems] = useState([]);
+    const [postId, setPostId] = useState(null);
+
+    const getSwapiData = fetch("https://swapi.dev/api/people/1")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setSwapiItems(result);
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                console.log(error);
+                setIsLoaded(true);
+            }
+        );
+
+    const postData = () => {
+        fetch('http://ptsv2.com/t/2npxg-1669132007/post', {
+            method: 'POST',
+            mode: "no-cors",
+            body: JSON.stringify({
+                // Add parameters here
+                'potato': 1
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                console.log('error: ' + err.message);
+            });
+    }
 
     // Note: the empty deps array [] means
     // this useEffect will run once
     // similar to componentDidMount()
     useEffect(() => {
-        fetch("https://swapi.dev/api/people/1")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setSwapiItems(result);
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    console.log(error);
-                    setIsLoaded(true);
-                }
-            )
     }, [])
 
     return (
@@ -37,6 +60,7 @@ function Api() {
                     {isLoaded ? swapiItems.name : "loading..."}
                 </ul>
             </h1>
+            <button onClick={postData}>Post some data</button>
             <Footer />
         </>
     );

6d690bbdf75625cf0a58a42a973605d80693fcf7
Ivy Dekker <ivy_dk@hotmail.com>
2022-11-22 15:37:21 +0100

    routing works

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
-import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import Environments from "./assets/Environment/Environment";
-import { Suspense } from "react";
-import { Environment, PresentationControls, View, Stars, OrbitControls } from "@react-three/drei";
-import { Physics, usePlane } from "@react-three/cannon";
+import React from "react";
 
+// for routing
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
-
-function Plane() {
-  const [ref, api] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0],
-    position: [0, -1, 0]
-  }));
-  return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightgreen" />
-    </mesh>
-  );
-}
+// Screens
+import Home from "./screens/HomeScreen";
+import Journey from "./screens/JourneyScreen";
 
 function App() {
-  return (
-    <div className="canvasContainer">
-      <div className="App">
-        <Canvas camera={{ position: [0, -0.2, 1.2] }}>
-          <OrbitControls target={[0, -0.4, 0]}/>
-          {/* <PresentationControls global zoom={4} rotation={[0, -Math.PI / 4, 0]} polar={[0, Math.PI / 4]}> */}
-            <Stars />
-            <ambientLight intensity={0.5} />
-            {/* <spotLight position={[10, 15, 10]} angle={0.3} /> */}
-            <Suspense fallback={null}>
-              <Physics>
-                <mesh position={[1.5, -1, 0]} scale={1}>
-                  <Environments />
-                </mesh>
-                <Plane />
-              </Physics>
-            </Suspense>
-            <Environment preset="sunset" />
-          {/* </PresentationControls> */}
-        </Canvas>
-      </div>
-      <div class="progress">
-        <div class="progress-value"><h3 className="level">Level 50</h3></div>
-      </div>
-      <div class="parent">
-        <button class="ButtonHome">&#9816;</button>
-        <button class="ButtonHome">&#9728;</button>
-        <button class="ButtonHome">&#9731;</button>
-      </div>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/journey" element={<Journey />} />
+        </Routes>
+    );
 }
 
 export default App;

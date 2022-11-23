fb1428182b3723a63595a214bf8c711227cdbebd
Mitchel0058 <goed0058@hz.nl>
2022-11-23 11:57:45 +0100

    input post working

--- src/pages/api/testApi.js
+++ src/pages/api/testApi.js
@@ -1,28 +1,33 @@
 import Footer from "../../components/footer/Footer";
 import Navigation from "../../components/navigation/Navigation";
-import React, { useState, setState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function Api() {
-    const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [swapiItems, setSwapiItems] = useState([]);
-    const [postId, setPostId] = useState(null);
+    const [userName, setUserName] = useState([]);
+    const [number, setNumber] = useState([]);
+    const [stuff, setStuff] = useState([]);
 
-    const getSwapiData = fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true);
-                setSwapiItems(result);
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-                console.log(error);
-                setIsLoaded(true);
-            }
-        );
+
+    function getSwapiData() {
+        fetch("https://swapi.dev/api/people/1")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setSwapiItems(result);
+                    setUserName(result.name);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    console.log(error);
+                    setIsLoaded(true);
+                }
+            );
+    }
 
     const postData = () => {
         fetch('http://ptsv2.com/t/2npxg-1669132007/post', {
@@ -30,7 +35,9 @@ function Api() {
             mode: "no-cors",
             body: JSON.stringify({
                 // Add parameters here
-                'potato': 1
+                'username': userName,
+                'number': number,
+                'stuff': stuff
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -49,21 +56,40 @@ function Api() {
     // this useEffect will run once
     // similar to componentDidMount()
     useEffect(() => {
+        getSwapiData();
     }, [])
 
     return (
         <>
             <Navigation />
             <h1 className="mt-5 pt-5">
-                Test
                 <ul>
                     {isLoaded ? swapiItems.name : "loading..."}
                 </ul>
             </h1>
+            <form>
+                <label>Enter your name:
+                    <input type="text"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)} />
+                </label>
+                <label>Enter a number:
+                    <input type="text" 
+                    value={number}
+                    onChange={(e) => setNumber(e.target.value)}/>
+                </label>
+                <label>Enter some stuff:
+                    <input type="text"
+                    value={stuff}
+                    onChange={(e) => setStuff(e.target.value)} />
+                </label>
+            </form>
             <button onClick={postData}>Post some data</button>
             <Footer />
         </>
     );
+
+
 }
 
 export default Api;

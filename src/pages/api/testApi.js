df9565501d74b9797f73fce8b4b32ec5ad336469
Mitchel0058 <goed0058@hz.nl>
2022-11-28 13:05:00 +0100

    visual update

--- src/pages/api/testApi.js
+++ src/pages/api/testApi.js
@@ -2,12 +2,19 @@ import Footer from "../../components/footer/Footer";
 import Navigation from "../../components/navigation/Navigation";
 import React, { useState, useEffect } from "react";
 
+// https://www.npmjs.com/package/react-activity
+import { Spinner } from "react-activity";
+import "react-activity/dist/library.css";
+
 function Api() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [swapiItems, setSwapiItems] = useState([]);
-    const [userName, setUserName] = useState([]);
-    const [number, setNumber] = useState([]);
-    const [stuff, setStuff] = useState([]);
+    const [userNamePost, setUserNamePost] = useState([]);
+    const [challengePost, setChallengePost] = useState([]);
+    const [somethingElsePost, setSomethingElsePost] = useState([]);
+    const [userNamePut, setUserNamePut] = useState([]);
+    const [challengePut, setChallengePut] = useState([]);
+    const [somethingElsePut, setSomethingElsePut] = useState([]);
 
 
     function getSwapiData() {
@@ -17,7 +24,7 @@ function Api() {
                 (result) => {
                     setIsLoaded(true);
                     setSwapiItems(result);
-                    setUserName(result.name);
+                    setUserNamePut(result.name);
                 },
                 // Note: it's important to handle errors here
                 // instead of a catch() block so that we don't swallow
@@ -25,19 +32,41 @@ function Api() {
                 (error) => {
                     console.log(error);
                     setIsLoaded(true);
-                }
-            );
+                });
     }
 
     const postData = () => {
-        fetch('http://ptsv2.com/t/2npxg-1669132007/post', {
+        fetch('http://ptsv2.com/t/c33xu-1669373343/post', {
             method: 'POST',
             mode: "no-cors",
             body: JSON.stringify({
                 // Add parameters here
-                'username': userName,
-                'number': number,
-                'stuff': stuff
+                'username': userNamePost,
+                'challenge': challengePost,
+                'something else': somethingElsePost
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                console.log('error: ' + err.message);
+            });
+    }
+
+    const putData = () => {
+        fetch('http://ptsv2.com/t/c33xu-1669373343/post', {
+            method: 'PUT',
+            mode: "no-cors",
+            body: JSON.stringify({
+                // Add parameters here
+                'username': userNamePut,
+                'challenge': challengePut,
+                'something else': somethingElsePut
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -60,36 +89,181 @@ function Api() {
     }, [])
 
     return (
-        <>
+        <div className="mx-3 my-5 py-5">
             <Navigation />
-            <h1 className="mt-5 pt-5">
-                <ul>
-                    {isLoaded ? swapiItems.name : "loading..."}
-                </ul>
+                <div className="title">
+                    Get example:
+                </div>
+                {isLoaded ?
+                    <div>
+                        <div>{swapiItems.name}</div>
+                        <div>mass: {swapiItems.mass}</div>
+                        <div>height: {swapiItems.height}</div>
+                    </div>
+                    :
+                    <Spinner />}
+
+            {/* ============================================================================================================================================ */}
+
+            <h1 className="title">
+                Post example:
             </h1>
-            <form>
-                <label>Enter your name:
-                    <input type="text"
-                        value={userName}
-                        onChange={(e) => setUserName(e.target.value)} />
-                </label>
-                <label>Enter a number:
-                    <input type="text" 
-                    value={number}
-                    onChange={(e) => setNumber(e.target.value)}/>
-                </label>
-                <label>Enter some stuff:
-                    <input type="text"
-                    value={stuff}
-                    onChange={(e) => setStuff(e.target.value)} />
-                </label>
+
+            <form action="">
+                <div class="field">
+                    <label class="label">Username</label>
+                    <div class="control has-icons-left
+                      has-icons-right">
+                        <input
+                            class="input is-success"
+                            type="text"
+                            placeholder="Enter your username"
+                            onChange={(e) => setUserNamePost(e.target.value)}
+                        />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Challenge</label>
+                    <div class="control has-icons-left
+                      has-icons-right">
+                        <textarea
+                            class="input"
+                            type="challenge"
+                            placeholder="Enter your challenge"
+                            onChange={(e) => setChallengePost(e.target.value)}
+                        />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-key"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">
+                        Something Else
+                    </label>
+                    <div class="control">
+                        <textarea
+                            class="textarea"
+                            placeholder="Want to tell anything?"
+                            onChange={(e) => setSomethingElsePost(e.target.value)}
+                        ></textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox" />
+                            I agree to the <a href="#">
+                                terms and conditions
+                            </a>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-success" onClick={postData}>
+                            Post
+                        </button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-link is-light">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
             </form>
-            <button onClick={postData}>Post some data</button>
-            <Footer />
-        </>
+
+
+            {/* ============================================================================================================================================ */}
+
+
+            <h1 className="title">Put example:</h1>
+
+            <form action="">
+                <div class="field">
+                    <label class="label">Username</label>
+                    <div class="control has-icons-left
+                      has-icons-right">
+                        <input
+                            class="input is-success"
+                            type="text"
+                            placeholder="Enter your username"
+                            value={userNamePut}
+                            onChange={(e) => setUserNamePut(e.target.value)}
+                        />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-user"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Challenge</label>
+                    <div class="control has-icons-left
+                      has-icons-right">
+                        <textarea
+                            class="input"
+                            type="challenge"
+                            placeholder="Enter your challenge"
+                            value={challengePut}
+                            onChange={(e) => setChallengePut(e.target.value)}
+                        />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-key"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">
+                        Something Else
+                    </label>
+                    <div class="control">
+                        <textarea
+                            class="textarea"
+                            placeholder="Want to tell anything?"
+                            onChange={(e) => setSomethingElsePut(e.target.value)}
+                        ></textarea>
+                    </div>
+                </div>
+
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox" />
+                            I agree to the <a href="#">
+                                terms and conditions
+                            </a>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-success" onClick={putData}>
+                            Put
+                        </button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-link is-light">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            {/* ============================================================================================================================================ */}
+
+            {/* <Footer /> */}
+        </div>
     );
-
-
 }
 
 export default Api;

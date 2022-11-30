6b51082435d4f49cfd9a9d8651a1676cc11fdccd
Mitchel0058 <goed0058@hz.nl>
2022-11-30 14:32:38 +0100

    variation select done besides models/canvas nedded

--- src/pages/chooseAvatar/VariationSelect.js
+++ src/pages/chooseAvatar/VariationSelect.js
@@ -12,33 +12,50 @@ import Dragon from '../../assets/Dragon/Dragon';
 
 function VariationSelect() {
     const [chosenAvatar, setChosenAvatar] = useState(undefined);
-    const [avatarVariation, setAvatarVariation] = useState([1, 2, 3, 4]);
-
-    function selectVariation(id) {
-        console.log(id);
-    }
 
     return (
         <>
+            {/* <Navigation /> */}
             <div className="pt-5 mt-5 mx-3">
                 <div className="title text-center has-text-centered">
                     Avatar customization
                 </div>
 
-                {avatarVariation.map((variation, i) =>
-                    <div className="columns is-mobile is-centered">
-                        <div className="box my-3 mr-3 mr-3 mr-3 column is-5" onClick={() => selectVariation(i)}>
-                            {"avatar variation " + variation}
-                        </div>
-                        <div className="box my-3 mr-3 mr-3 mr-3 column is-5" onClick={() => selectVariation(i)}>
-                            {"avatar variation " + variation}
-                        </div>
-                    </div>
-                )}
+                <div>
+                    {mapAvatarVariation()}
+                </div>
 
             </div>
         </>
     );
+}
+
+function selectVariation(id) {
+    console.log(id);
+}
+
+function mapAvatarVariation() {
+    const avatarVariation = ["{INSERT AVATAR VARIATION CANVAS HERE}", 2, 3, 4, 5, 6, 7, "hello there", "haAAA", 27, -3];
+    const mappedAvatarVariation = [];
+    for (let i = 0; i < avatarVariation.length; i++) {
+        mappedAvatarVariation.push(<>
+            <div className="columns is-mobile is-centered">
+                <div className="box my-3 mr-3 mr-3 mr-3 column is-5" onClick={() => selectVariation(i)}>
+                    {avatarVariation[i]}
+                </div>
+                <div hidden>{i++}</div>
+                {i >= avatarVariation.length ?
+                    ""
+                    :
+                    <div className="box my-3 mr-3 mr-3 mr-3 column is-5" onClick={() => selectVariation(i + 1)}>
+                        {avatarVariation[i]}
+                    </div>
+                }
+
+            </div>
+        </>);
+    }
+    return mappedAvatarVariation;
 }
 
 export default VariationSelect;

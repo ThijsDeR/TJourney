b35a39e829ec7e51d2c39127ed9a8464e1b2fde5
Jasonjason10 <ofil0001@hz.nl>
2022-12-05 11:38:07 +0100

    added arrows to categories

--- src/screens/ChallengeScreen.js
+++ src/screens/ChallengeScreen.js
@@ -1,6 +1,6 @@
 import Footer from "../components/footer/Footer";
 import Navigation from "../components/navigation/Navigation"
-
+import  ArrowRightpng from "../assets/Icons/arrow_forward_ios_FILL0_wght400_GRAD0_opsz48.png"
 
 
 const divStyle = {
@@ -27,15 +27,27 @@ function ChallengeScreen() {
             </div>
             <div style={divStyle}>
                 <h2>Nutrition</h2>
+                <div style={{marginLeft:"auto"}}>
+                <img src={ArrowRightpng} width="40" style={{maxHeight: "50px"}} alt="" />
+                </div>
             </div>
             <div style={divStyle}>
                 <h2>Sleep</h2>
+                <div style={{marginLeft:"auto"}}>
+                <img src={ArrowRightpng} width="40" style={{maxHeight: "50px"}} alt="" />
+                </div>
             </div>
             <div style={divStyle}>
                 <h2>Screentime</h2>
+                <div style={{marginLeft:"auto"}}>
+                <img src={ArrowRightpng} width="40" style={{maxHeight: "50px"}} alt="" />
+                </div>
             </div>
             <div style={divStyle}>
                 <h2>Activity</h2>
+                <div style={{marginLeft:"auto"}}>
+                <img src={ArrowRightpng} width="40" style={{maxHeight: "50px"}} alt="" />
+                </div>
             </div>
 
         </div>

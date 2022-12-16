215d01aadedd6b3f49d1dd8c02ff7526c2a6b2d3
ThijsDeR <rech0004@hz.nl>
2022-12-16 13:23:00 +0100

    merge fixes

--- src/pages/community/CommunityScreen.js
+++ src/pages/community/CommunityScreen.js
@@ -1,34 +1,20 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
-import Footer from "../../components/footer/Footer";
 import Navigation from "../../components/navigation/Navigation";
-
-import { getAllChallenges } from "../../services/goal-service.js";
-import { getGameSession, setSteps } from "../../services/game-service.js";
-import { Navigate } from 'react-router-dom';
-import Loading from '../../components/loading/Loading';
-import { calculateLevel } from '../../services/level-service';
-import { getCurrentUser } from '../../services/auth-service';
 
 // styling
 import 'bulma/css/bulma.min.css';
-import { pageStyle, title, primaryColor, tabs, tabPanel, tab, appContainer } from '../../styling/StylingVariables.js';
-
-// icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown, faCircle, faAngleRight, faSquare } from '@fortawesome/free-solid-svg-icons'
+import { pageStyle, appContainer } from '../../styling/StylingVariables.js';
 
 // tabs
 import Tabs from '../../components/tabs/Tabs';
-import Tab from '../../components/tabs/Tab';
 
 // tab content
 import Friends from './Friends.js';
 import Groups from './Groups.js';
 import Leaderboard from './Leaderboard.js';
 
-function CommunityScreen({ user, setUser, timeElapsed, isLoading, setIsLoading }) {
+function CommunityScreen({ user }) {
     return (
         <>
             <div style={pageStyle}>

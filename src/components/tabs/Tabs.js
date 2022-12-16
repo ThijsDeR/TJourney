352a45b77b2a0198d948bd73c5ad9e0303620f49
Ivy Dekker <ivy_dk@hotmail.com>
2022-12-16 11:32:06 +0100

    Revert "refactor to functional component"
    This reverts commit cef64d2f59db64d8bacdd3181d9cc11de7a4f42f.

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,58 +1,62 @@
-import React, { Component, useState, children, setState } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Tab from "./Tab";
 import { tabListItemContainer, tabList, tabContent, tabListItem, centerDiv } from "../../styling/StylingVariables.js"
 
-function Tabs() {
-    // checks if the children are an array
-    const propTypes = {
-        // children is an array of objects of type Tab
+class Tabs extends Component {
+    static propTypes = {
         children: PropTypes.instanceOf(Array).isRequired,
     };
 
-    // sets the active tab to the first tab
-    const [activeTab, setActiveTab] = useState(props.children[0].props.label);
+    constructor(props) {
+        super(props);
 
-    // set active tab to the tab that was clicked
-    const onClickTabItem = (tab) => {
-        setActiveTab(tab);
-    };
-
-    const props = {
-        children: PropTypes.instanceOf(Array).isRequired,
+        this.state = {
+            activeTab: this.props.children[0].props.label,
+        };
     }
 
-    // returns the tabs
-    return (
-        <div className="tabs" style={tabListItemContainer}>
-            <div className="item" style={tabList}>
-                <div style={centerDiv}>
-                    <ol className="tab-list" style={tabListItem}>
-                        {/* returns all the tab items as a li */}
-                        {children.map((child) => {
-                            const { label } = child.props;
+    onClickTabItem = (tab) => {
+        this.setState({ activeTab: tab });
+    };
 
-                            return (
-                                <Tab
-                                    activeTab={activeTab}
-                                    key={label}
-                                    label={label}
-                                    onClick={onClickTabItem}
-                                />
-                            );
-                        })}
-                    </ol>
+    render() {
+        const {
+            onClickTabItem,
+            props: { children },
+            state: { activeTab },
+        } = this;
+
+        return (
+            <div className="tabs" style={tabListItemContainer}>
+                <div className="item" style={tabList}>
+                    <div style={centerDiv}>
+                        <ol className="tab-list" style={tabListItem}>
+                            {/* returns all the tab items as a li */}
+                            {children.map((child) => {
+                                const { label } = child.props;
+
+                                return (
+                                    <Tab
+                                        activeTab={activeTab}
+                                        key={label}
+                                        label={label}
+                                        onClick={onClickTabItem}
+                                    />
+                                );
+                            })}
+                        </ol>
+                    </div>
+                </div>
+                <div className="tab-content" style={tabContent}>
+                    {children.map((child) => {
+                        if (child.props.label !== activeTab) return undefined;
+                        return child.props.children;
+                    })}
                 </div>
             </div>
-            <div className="tab-content" style={tabContent}>
-                {children.map((child) => {
-                    // if the tab is not active, return undefined (nothing)
-                    if (child.props.label !== activeTab) return undefined;
-                    return child.props.children;
-                })}
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Tabs;

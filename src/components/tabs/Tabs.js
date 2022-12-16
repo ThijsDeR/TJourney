cef64d2f59db64d8bacdd3181d9cc11de7a4f42f
Ivy Dekker <ivy_dk@hotmail.com>
2022-12-16 11:25:26 +0100

    refactor to functional component

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,62 +1,58 @@
-import React, { Component } from "react";
+import React, { Component, useState, children, setState } from "react";
 import PropTypes from "prop-types";
 import Tab from "./Tab";
 import { tabListItemContainer, tabList, tabContent, tabListItem, centerDiv } from "../../styling/StylingVariables.js"
 
-class Tabs extends Component {
-    static propTypes = {
+function Tabs() {
+    // checks if the children are an array
+    const propTypes = {
+        // children is an array of objects of type Tab
         children: PropTypes.instanceOf(Array).isRequired,
     };
 
-    constructor(props) {
-        super(props);
+    // sets the active tab to the first tab
+    const [activeTab, setActiveTab] = useState(props.children[0].props.label);
 
-        this.state = {
-            activeTab: this.props.children[0].props.label,
-        };
-    }
-
-    onClickTabItem = (tab) => {
-        this.setState({ activeTab: tab });
+    // set active tab to the tab that was clicked
+    const onClickTabItem = (tab) => {
+        setActiveTab(tab);
     };
 
-    render() {
-        const {
-            onClickTabItem,
-            props: { children },
-            state: { activeTab },
-        } = this;
+    const props = {
+        children: PropTypes.instanceOf(Array).isRequired,
+    }
 
-        return (
-            <div className="tabs" style={tabListItemContainer}>
-                <div className="item" style={tabList}>
-                    <div style={centerDiv}>
-                        <ol className="tab-list" style={tabListItem}>
-                            {/* returns all the tab items as a li */}
-                            {children.map((child) => {
-                                const { label } = child.props;
+    // returns the tabs
+    return (
+        <div className="tabs" style={tabListItemContainer}>
+            <div className="item" style={tabList}>
+                <div style={centerDiv}>
+                    <ol className="tab-list" style={tabListItem}>
+                        {/* returns all the tab items as a li */}
+                        {children.map((child) => {
+                            const { label } = child.props;
 
-                                return (
-                                    <Tab
-                                        activeTab={activeTab}
-                                        key={label}
-                                        label={label}
-                                        onClick={onClickTabItem}
-                                    />
-                                );
-                            })}
-                        </ol>
-                    </div>
-                </div>
-                <div className="tab-content" style={tabContent}>
-                    {children.map((child) => {
-                        if (child.props.label !== activeTab) return undefined;
-                        return child.props.children;
-                    })}
+                            return (
+                                <Tab
+                                    activeTab={activeTab}
+                                    key={label}
+                                    label={label}
+                                    onClick={onClickTabItem}
+                                />
+                            );
+                        })}
+                    </ol>
                 </div>
             </div>
-        );
-    }
+            <div className="tab-content" style={tabContent}>
+                {children.map((child) => {
+                    // if the tab is not active, return undefined (nothing)
+                    if (child.props.label !== activeTab) return undefined;
+                    return child.props.children;
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default Tabs;

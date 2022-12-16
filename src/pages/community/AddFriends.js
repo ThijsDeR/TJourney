fa7b51cb52d63e546fc0b34e999a8ac0f9318167
Ivy Dekker <ivy_dk@hotmail.com>
2022-12-16 15:02:41 +0100

    search works

--- src/pages/community/AddFriends.js
+++ src/pages/community/AddFriends.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Navigation from "../../components/navigation/Navigation";
 
@@ -15,11 +15,43 @@ import Groups from './Groups.js';
 import Leaderboard from './Leaderboard.js';
 
 function AddFriends({ user }) {
+
+    const [searchInput, setSearchInput] = useState("");
+
+    //  test data
+    const users = [
+        { name: "John", level: 1, id: 1 },
+        { name: "Jane", level: 2, id: 2 },
+        { name: "Jack", level: 3, id: 3 },
+        { name: "Jill", level: 4, id: 4 },
+        { name: "Jenny", level: 5, id: 5 },
+        { name: "Jen", level: 6, id: 6 },
+        { name: "Jenna", level: 7, id: 7 },
+    ];
+
+    const handleSearchInput = (e) => {
+        //  prevent page refresh
+        e.preventDefault();
+        setSearchInput(e.target.value);
+        console.log(searchInput);
+    }
+
+    const searchResults = users.filter(user => user.name.toLowerCase().includes(searchInput.toLowerCase()));
+
     return (
         <>
             <div style={pageStyle}>
                 <div style={appContainer}>
-                    Add friends
+                    <input
+                        type="text"
+                        placeholder="Search here"
+                        onChange={handleSearchInput}
+                        value={searchInput} />
+                    {searchResults.map(user => (
+                        <div key={user.id}>
+                            <p>{user.name}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
             <Navigation user={user} />

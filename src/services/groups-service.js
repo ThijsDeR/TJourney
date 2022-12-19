48665821bd1ff1e7a390bfba3cabd7613c7ab881
Jessevdbos <bos0038@hz.nl>
2022-12-19 14:18:02 +0100

    groups added to groups page

--- src/services/groups-service.js
+++ src/services/groups-service.js
@@ -1,78 +1,104 @@
 import axios from "../api/axios";
 import { getCurrentUser } from "./auth-service";
 
-export const getGroups = async () => {
+// export const getGroups = async () => {
+//     const localUser = JSON.parse(localStorage.getItem("user"))
+
+//     const result = await getCurrentUser()
+//     console.log(result);
+//     if (result) {
+//         const promises = [];
+//         console.log(result._id);
+//         result.groups.forEach((group) => {
+//             console.log(group)
+//             promises.push(axios.get("/v1/groups"+ result.group.groupName, {
+//                 headers: { Authorization: `Bearer ${localUser.accessToken}` }
+//             }).then(async (response) => {
+//                 if (response.data.error) throw response.data.error
+
+//                 const user = await axios.get("/v1/groups"+ result.group.groupName, {
+//                     headers: { Authorization: `Bearer ${localUser.accessToken}` }
+//                 }).then((response) => {
+                    
+//                     if (response.data.error) throw response.data.error
+                    
+//                     return response.data.data;
+//                 })
+
+
+//                 return { user: user, gameSession: response.data.data };
+//             }));
+//         })
+
+//         return await Promise.all(promises);
+//     }
+
+//     return null
+// }
+
+// export const addGroup = async (groupId) => {
+//     const localUser = JSON.parse(localStorage.getItem("user"))
+
+//     return await axios.put("/v1/users", {
+//         updateQuery: {
+//             $push: {
+//                 groupID: groupId
+//             }
+//         },
+//     }, {
+//         headers: { Authorization: `Bearer ${localUser.accessToken}` }
+//     }).then(async (response) => {
+//         if (response.data.error) throw response.data.error
+
+//         return response.data.data;
+//     })
+
+// }
+
+// export const removeGroup = async (userId, name, groupImage) => {
+//     const localUser = JSON.parse(localStorage.getItem("user"))
+
+//     return await axios.put("/v1/users", {
+//         updateQuery: {
+//             $pull: {
+//                 groupName: name,
+//                 member: userId,
+//                 image: groupImage
+//             }
+//         },
+//     }, {
+//         headers: { Authorization: `Bearer ${localUser.accessToken}` }
+//     }).then(async (response) => {
+//         if (response.data.error) throw response.data.error
+
+//         return response.data.data;
+//     })
+// }
+
+export const createGroups = (name, description, members, admin, image) => {
     const localUser = JSON.parse(localStorage.getItem("user"))
 
-    const result = await getCurrentUser()
-    console.log(result);
-    if (result) {
-        const promises = [];
-        result.groups.forEach((group) => {
-            promises.push(axios.get("/v1/gameSessions/user/" + group, {
-                headers: { Authorization: `Bearer ${localUser.accessToken}` }
-            }).then(async (response) => {
-                if (response.data.error) throw response.data.error
-
-                const user = await axios.get("/v1/users/" + group, {
-                    headers: { Authorization: `Bearer ${localUser.accessToken}` }
-                }).then((response) => {
-                    
-                    if (response.data.error) throw response.data.error
-                    
-                    return response.data.data;
-                })
-
-
-                return { user: user, gameSession: response.data.data };
-            }));
-        })
-
-        return await Promise.all(promises);
-    }
-
-    return null
-}
-
-export const addGroup = async (name, userId, groupImage) => {
-    const localUser = JSON.parse(localStorage.getItem("user"))
-
-    return await axios.put("/v1/users", {
-        updateQuery: {
-            $push: {
-                groups: {
-                    groupName: name,
-                    member: userId,
-                    image: groupImage
-                }
-            }
-        },
+    return axios.post("/v1/groups", {
+        name,
+        description,
+        members,
+        admin,
+        image
     }, {
         headers: { Authorization: `Bearer ${localUser.accessToken}` }
-    }).then(async (response) => {
+    }).then((response) => {
         if (response.data.error) throw response.data.error
 
         return response.data.data;
-    })
+    });
+};
 
-}
-
-export const removeGroup = async (userId, name, groupImage) => {
+export const getAllGroups = async () => {
     const localUser = JSON.parse(localStorage.getItem("user"))
 
-    return await axios.put("/v1/users", {
-        updateQuery: {
-            $pull: {
-                groupName: name,
-                member: userId,
-                image: groupImage
-            }
-        },
-    }, {
+    const groups = await axios.get("/v1/groups/", {
         headers: { Authorization: `Bearer ${localUser.accessToken}` }
-    }).then(async (response) => {
-        if (response.data.error) throw response.data.error
-
-        return response.data.data;
     })
+
+    return groups.data.data
 }

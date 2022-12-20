98c91895e1846ce57e0477013b6fc26adc6e7c0e
ThijsDeR <rech0004@hz.nl>
2022-12-20 09:48:43 +0100

    Friends Link

--- src/pages/community/AddFriends.js
+++ src/pages/community/AddFriends.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Navigation from "../../components/navigation/Navigation";
 import { Link, Navigate } from "react-router-dom";
@@ -18,21 +18,33 @@ import Leaderboard from './Leaderboard.js';
 // icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { addFriend, getNonFriends } from '../../services/friends-service';
+import { calculateLevel } from '../../services/level-service';
 
 function AddFriends({ user }) {
-
     const [searchInput, setSearchInput] = useState("");
+    const [nonFriends, setNonFriends] = useState(undefined)
 
     //  test data
-    const users = [
-        { name: "John", level: 1, id: 1 },
-        { name: "Jane", level: 2, id: 2 },
-        { name: "Jack", level: 3, id: 3 },
-        { name: "Jill", level: 4, id: 4 },
-        { name: "Jenny", level: 5, id: 5 },
-        { name: "Jen", level: 6, id: 6 },
-        { name: "Jenna", level: 7, id: 7 },
-    ];
+    // const users = [
+    //     { name: "John", level: 1, id: 1 },
+    //     { name: "Jane", level: 2, id: 2 },
+    //     { name: "Jack", level: 3, id: 3 },
+    //     { name: "Jill", level: 4, id: 4 },
+    //     { name: "Jenny", level: 5, id: 5 },
+    //     { name: "Jen", level: 6, id: 6 },
+    //     { name: "Jenna", level: 7, id: 7 },
+    // ];
+
+    useEffect(() => {
+        getNonFriends().then((nonFriends) => {
+            nonFriends.forEach((nonFriend) => {
+                nonFriend.level = calculateLevel(nonFriend.level.amount)
+            })
+            setNonFriends(nonFriends)
+            console.log(nonFriends)
+        })
+    }, [])
 
     const handleSearchInput = (e) => {
         //  prevent page refresh
@@ -40,8 +52,6 @@ function AddFriends({ user }) {
         setSearchInput(e.target.value);
     }
 
-    // TODO: remove all users that are already friends
-    const searchResults = users.filter(user => user.name.toLowerCase().includes(searchInput.toLowerCase()));
 
     return (
         <>
@@ -61,35 +71,39 @@ function AddFriends({ user }) {
                         placeholder="Search"
                         onChange={handleSearchInput}
                         value={searchInput} />
-                    {searchResults.map(user => (
-                        <div key={user.id}>
-                            <div style={chatContainer}>
-                                <div className='friendTile' style={{ ...friendsTile, ...{ margin: 'unset' } }}>
-                                    <div className='friendItems' style={friendItems}>
-                                        <div className='friendIcon' style={fakePF}></div>
-                                        <div className='friendInfo'>
-                                            <div className='friendName' style={{ fontWeight: 'bold' }}> {user.name} </div>
-                                            <div className='friendLevel' style={{ fontWeight: 'lighter' }}> Level {user.level} </div>
+                    <div style={{overflowY: "auto", maxHeight: "100%"}}>
+                        {nonFriends ? nonFriends.map(user => (
+                            <div className='userDiv' key={user._id}>
+                                <div style={chatContainer}>
+                                    <div className='friendTile' style={{ ...friendsTile, ...{ margin: 'unset' } }}>
+                                        <div className='friendItems' style={friendItems}>
+                                            <div className='friendIcon' style={fakePF}></div>
+                                            <div className='friendInfo'>
+                                                <div className='friendName' style={{ fontWeight: 'bold' }}> {user.username} </div>
+                                                <div className='friendLevel' style={{ fontWeight: 'lighter' }}> Level {user.level.level} </div>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    {/* TODO: add friend when button is clicked */}
-                                    <div style={{
-                                        justifySelf: 'flex-end',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                    }}>
-                                        <Link style={unsetLinkStyle}>
-                                            <div style={{ ...smallButton, ...{ width: '70px' } }}>Add</div>
-                                        </Link>
+                                        {/* TODO: add friend when button is clicked */}
+                                        <div style={{
+                                            justifySelf: 'flex-end',
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                        }}>
+                                            <a href style={unsetLinkStyle} onClick={(e) => {
+                                                addFriend(user._id)
+                                                e.target.closest(".userDiv").remove()
+                                            }}>
+                                                <div style={{ ...smallButton, ...{ width: '70px' } }}>Add</div>
+                                            </a>
+                                        </div>
+
                                     </div>
 
                                 </div>
-
                             </div>
-                        </div>
-                    ))
-                    }
+                        )) : ""}
+                    </div>
                 </div >
             </div >
             <Navigation user={user} />

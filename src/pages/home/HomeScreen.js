ec28b0a27a4e548411abf9346c8931490effd65d
Ivy Dekker <ivy_dk@hotmail.com>
2023-01-10 15:46:06 +0100

    update HomeScreen.js
    account button + center name, level

--- src/pages/home/HomeScreen.js
+++ src/pages/home/HomeScreen.js
@@ -31,8 +31,8 @@ function Home({ user, setCurrentUser, isLoading, setIsLoading }) {
         return <Navigate to="/login" replace />;
     }
 
-    if(!isLoading) {
-        if(user.tutorialFinished === undefined || user.tutorialFinished === false) {
+    if (!isLoading) {
+        if (user.tutorialFinished === undefined || user.tutorialFinished === false) {
             return <Navigate to="/tutorial" replace />;
         }
     }
@@ -51,9 +51,15 @@ function Home({ user, setCurrentUser, isLoading, setIsLoading }) {
                     <>
                         <div style={{ position: "fixed", top: "0px", bottom: "0px", left: "0px", right: "0px" }}>
                             <section className="bg-image" style={{ height: "100%" }}>
-                                <div style={{ display: "grid", gap: "5%", gridTemplateColumns: "10% 85%" }}>
-                                    <Link to="/account" style={{ color: "black", width: "100%", height: "100%", display: "flex", justifyContent: "center", flexDirection: "column" }}><FontAwesomeIcon icon={faUser} style={{ marginLeft: "10%", width: "80%", height: "80%" }} /></Link>
-                                    <h2 className="is-size-3 has-text-weight-bold">{user ? user.username : ""} ({level ? `${level.level} (${level.xp} / ${level.neededXP})` : ""} )</h2>
+
+                                <div style={{ display: "grid", gap: "5%", gridTemplateColumns: "15% 85%" }}>
+                                    <Link to="/account" style={{ color: "black", marginTop: "10px", width: "2rem", height: "2rem", display: "flex", justifyContent: "center", flexDirection: "column" }}>
+                                        <FontAwesomeIcon icon={faUser} style={{ marginLeft: "50%", width: "100%", height: "100%" }} />
+                                    </Link>
+
+                                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                        <h2 className="is-size-3 has-text-weight-bold" style={{ marginRight: "30%" }}>{user ? user.username : ""} ({level ? `${level.level} (${level.xp} / ${level.neededXP})` : ""} )</h2>
+                                    </div>
                                 </div>
 
                                 <div className="is-flex is-justify-content-center">

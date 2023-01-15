eb2beb244ada1a50986ae8c236d0ef6b44be17db
ThijsDeR <rech0004@hz.nl>
2023-01-15 23:44:54 +0100

    Chat scroll

--- src/pages/community/FriendChat.js
+++ src/pages/community/FriendChat.js
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import ChatInput from '../../components/chat/ChatInput.js';
 import Navigation from '../../components/navigation/Navigation.js';
+import "./chatStyle.css";
 
 let timer = 0;
 export default function FriendChat({ user, setIsLoading, isLoading }) {
@@ -30,6 +31,7 @@ export default function FriendChat({ user, setIsLoading, isLoading }) {
     useEffect(() => {
         getUser(searchParams.get("id")).then((user) => {
             setFriend(user)
+            getMessages()
         })
     }, [])
 
@@ -101,24 +103,24 @@ export default function FriendChat({ user, setIsLoading, isLoading }) {
                     <Link to="/community" style={{ color: user.preferences.style.secondaryColor, margin: "0 25px" }}><FontAwesomeIcon icon={faChevronLeft} /></Link>
                     <h3 style={{ margin: "0 25px" }}>{friend.username}</h3>
                 </div>
-                <div style={{position: "fixed", top: "50px", left: "0", right: "0", bottom: "135px", display: "flex", flexDirection: "column", justifyContent: "end", gap: "10px", overflowY: "auto"}}>
+                <div className='chatContainer' style={{ position: "fixed", top: "50px", left: "0", right: "0", bottom: "135px", display: "flex", flexDirection: "column-reverse", overflowY: "auto" }}>
                     {
                         messages ? messages.map((message) => (
-                                <div key={messages._id} style={{display: "flex", justifyContent: "end"}}>
-                                    <div style={{margin: "0 25px", padding: "5px 20px", borderRadius: "25px", backgroundColor: user.preferences.style.primaryColor, color: user.preferences.style.textColor}}>
-                                        <div >
-                                            <p>{message.message}</p>
-                                        </div>
+                            <div key={messages._id} style={{ display: "flex", justifyContent: "end" }}>
+                                <div style={{ margin: "0 25px", padding: "5px 20px", borderRadius: "25px", backgroundColor: user.preferences.style.primaryColor, color: user.preferences.style.textColor }}>
+                                    <div >
+                                        <p>{message.message}</p>
                                     </div>
                                 </div>
-                            )
+                            </div>
+                        )
                         ) : ''
                     }
                 </div>
-                <div style={{position: "fixed", bottom: "75px", left: "0", right: "0", height: "50px", display: "flex", justifyContent: "space-around", alignItems: "center", backgroundColor: user.preferences.style.primaryColor, color: user.preferences.style.textColor}}>
+                <div style={{ position: "fixed", bottom: "75px", left: "0", right: "0", height: "50px", display: "flex", justifyContent: "space-around", alignItems: "center", backgroundColor: user.preferences.style.primaryColor, color: user.preferences.style.textColor }}>
                     {/* <ChatInput handleSendMsg={handleSendMsg} /> */}
-                    <input type="text" value={msg} onChange={(e) => { setMsg(e.target.value) }} style={{border: "none", height: "80%", width: "75%", borderRadius: "20px", padding: "0 10px", backgroundColor: user.preferences.style.secondaryColor, color: user.preferences.style.textColor}}/>
-                    <FontAwesomeIcon icon={faPaperPlane} style={{color: user.preferences.style.secondaryColor }} onClick={sendChat}/>
+                    <input type="text" value={msg} onChange={(e) => { setMsg(e.target.value) }} style={{ border: "none", height: "80%", width: "75%", borderRadius: "20px", padding: "0 10px", backgroundColor: user.preferences.style.secondaryColor, color: user.preferences.style.textColor }} />
+                    <FontAwesomeIcon icon={faPaperPlane} style={{ color: user.preferences.style.secondaryColor }} onClick={sendChat} />
                 </div>
                 <Navigation user={user} />
             </>}
